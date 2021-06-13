7bbc951805f43bf510c9f117696cafae92b4a2a2
Vladimir <Zionit27@gmail.com>
2021-06-13 18:31:25 +0700

    add Class StructureRepository.java.

---
+++ app/src/main/java/ru/geekbrains/mynotes/domain/StructureRepository.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.mynotes.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StructureRepository {
+    public List<Structure> getStructures() {
+        ArrayList<Structure> result = new ArrayList<>();
+        return result;
+    }
+}
