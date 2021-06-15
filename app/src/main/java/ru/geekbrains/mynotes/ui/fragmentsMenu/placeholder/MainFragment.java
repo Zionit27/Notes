957d081a864d37ebfbdb2e45218a0aeab7decd9f
Vladimir <Zionit27@gmail.com>
2021-06-15 16:01:35 +0700

    add fragment MainFragment.

---
+++ app/src/main/java/ru/geekbrains/mynotes/ui/fragmentsMenu/placeholder/MainFragment.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.mynotes.ui.fragmentsMenu.placeholder;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.geekbrains.mynotes.R;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
