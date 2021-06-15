dca9bafe218d596d99f283f72f71cc4cf61b3179
Vladimir <Zionit27@gmail.com>
2021-06-15 16:04:59 +0700

    edit MainActivity.java.

--- app/src/main/java/ru/geekbrains/mynotes/ui/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mynotes/ui/MainActivity.java
@@ -1,22 +1,179 @@
 package ru.geekbrains.mynotes.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import ru.geekbrains.mynotes.R;
 import ru.geekbrains.mynotes.domain.Structure;
+import ru.geekbrains.mynotes.ui.fragmentsMenu.placeholder.FavoriteFragment;
+import ru.geekbrains.mynotes.ui.fragmentsMenu.placeholder.MainFragment;
+import ru.geekbrains.mynotes.ui.fragmentsMenu.placeholder.SettingsFragment;
 import ru.geekbrains.mynotes.ui.placeholder.ListFragment;
+import ru.geekbrains.mynotes.domain.Settings;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnNoteClicked {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+        readSettings();
+        initView();
     }
+
+    private void initView() {
+        initButtonMain();
+        initButtonFavorite();
+        initButtonSettings();
+        initToolbar();
+    }
+    private void initButtonSettings() {
+        Button buttonSettings = findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new SettingsFragment());
+            }
+        });
+    }
+
+    private void initButtonFavorite() {
+        Button buttonFavorite = findViewById(R.id.buttonFavorite);
+        buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new FavoriteFragment());
+            }
+        });
+    }
+
+    private void initButtonMain() {
+        Button buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new MainFragment());
+            }
+        });
+    }
+
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                addFragment(new SettingsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoriteFragment());
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    private void addFragment(Fragment fragment) {
+
+        //Получить менеджер фрагментов
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Открыть транзакцию
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Удалить видимый фрагмент
+        if (Settings.isDeleteBeforeAdd) {
+            Fragment fragmentToRemove = getVisibleFragment(fragmentManager);
+            if (fragmentToRemove != null) {
+                fragmentTransaction.remove(fragmentToRemove);
+            }
+        }
+
+        // Добавить фрагмент
+        if (Settings.isAddFragment) {
+            fragmentTransaction.add(R.id.container, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.container, fragment);
+        }
+
+        // Добавить транзакцию в бэкстек
+        if (Settings.isBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        // Закрыть транзакцию
+        fragmentTransaction.commit();
+    }
+
+    private Fragment getVisibleFragment(FragmentManager fragmentManager) {
+        List<Fragment> fragments = fragmentManager.getFragments();
+        int countFragments = fragments.size();
+        for (int i = countFragments - 1; i >= 0; i--) {
+            Fragment fragment = fragments.get(i);
+            if (fragment.isVisible())
+                return fragment;
+        }
+        return null;
+    }
+
+    private void readSettings() {
+        // Специальный класс для хранения настроек
+        SharedPreferences sharedPref = getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, MODE_PRIVATE);
+        // Считываем значения настроек
+        Settings.isBackStack = sharedPref.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isAddFragment = sharedPref.getBoolean(Settings.IS_ADD_FRAGMENT_USED, true);
+        Settings.isBackAsRemove = sharedPref.getBoolean(Settings.IS_BACK_AS_REMOVE_FRAGMENT, true);
+        Settings.isDeleteBeforeAdd = sharedPref.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+    }
+
 
     @Override
     public void onNoteClicked(Structure structure) {
